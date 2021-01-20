b4cec0046ab94caa38ba3a0950eb99414aefca6a
omri wallach <omriwallach1992@gmail.com>
2021-01-20 15:58:57 +0100

    transform categorial data ann

--- ann/main.py
+++ ann/main.py
@@ -1,7 +1,20 @@
 import pandas as pd
-import tensorflow as tf
 
 # Get dataset
 dataset = pd.read_csv("Churn_Modelling.csv")
 X = dataset.iloc[:,3:-1].values
-Y = dataset.iloc[:,-1].values
+Y = dataset.iloc[:, -1].values
+
+# Label Encoding categorial data
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+# One hot encoding 
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import numpy as np
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
