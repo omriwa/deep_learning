96c37e2fdbd7512fe6254ac43ac91421a8ef48c8
omri wallach <omriwallach1992@gmail.com>
2021-01-20 16:02:26 +0100

    ann features scaling

--- ann/main.py
+++ ann/main.py
@@ -21,4 +21,11 @@ X = np.array(ct.fit_transform(X))
 
 # Splitting the dataset
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train, Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
