85e1e50a726bf0d8a20a7e285603343043820b7e
omri wallach <omriwallach1992@gmail.com>
2021-01-21 15:24:07 +0100

    CNN init

---
+++ cnn/main.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+# Get dataset
+dataset = pd.read_csv("Churn_Modelling.csv")
+X = dataset.iloc[:,3:-1].values
+Y = dataset.iloc[:, -1].values
+
+# Label Encoding categorial data
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+# One hot encoding 
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import numpy as np
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# Splitting the dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Init CNN
+import tensorflow as tf
+
+ann = tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+ann.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+# Train CNN
+ann.fit(X_train, Y_train, batch_size=32, epochs=20)
+
+# Predict
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+# Checking accuracy
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(Y_test, y_pred)
+accuracy = accuracy_score(Y_test, y_pred)
+print('Matrix:\n',cm,'\n','Accuracy:\n',accuracy)
