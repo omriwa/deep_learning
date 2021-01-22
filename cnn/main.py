4a72e66add6a49673ccbb4d2e390b098dc986dbc
omri wallach <omriwallach1992@gmail.com>
2021-01-22 16:56:45 +0100

    create cnn data set

--- cnn/main.py
+++ cnn/main.py
@@ -1,53 +1,25 @@
-import pandas as pd
-
-# Get dataset
-dataset = pd.read_csv("Churn_Modelling.csv")
-X = dataset.iloc[:,3:-1].values
-Y = dataset.iloc[:, -1].values
-
-# Label Encoding categorial data
-from sklearn.preprocessing import LabelEncoder
-
-le = LabelEncoder()
-X[:, 2] = le.fit_transform(X[:, 2])
-
-# One hot encoding 
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-import numpy as np
-
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-
-# Splitting the dataset
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-
-# Feature scaling
-from sklearn.preprocessing import StandardScaler
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Init CNN
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
 
-ann = tf.keras.models.Sequential()
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
-ann.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
 
-# Train CNN
-ann.fit(X_train, Y_train, batch_size=32, epochs=20)
+training_set = train_datagen.flow_from_directory(
+    'dataset/training_set',
+    target_size=(64,64),
+    batch_size=32,
+    class_mode='binary'
+)
 
-# Predict
-y_pred = ann.predict(X_test)
-y_pred = (y_pred > 0.5)
-# Checking accuracy
-from sklearn.metrics import confusion_matrix, accuracy_score
+test_dataget = ImageDataGenerator(rescale=1./255)
 
-cm = confusion_matrix(Y_test, y_pred)
-accuracy = accuracy_score(Y_test, y_pred)
-print('Matrix:\n',cm,'\n','Accuracy:\n',accuracy)
+test_set = train_datagen.flow_from_directory(
+    'dataset/test_set',
+    target_size=(64,64),
+    batch_size=32,
+    class_mode='binary'
+)
