286650c76818cb1e01e42119b25bb9ee3fb39180
omri wallach <omriwallach1992@gmail.com>
2021-01-25 10:04:30 +0100

    cnn build

--- cnn/main.py
+++ cnn/main.py
@@ -23,3 +23,26 @@ test_set = train_datagen.flow_from_directory(
     batch_size=32,
     class_mode='binary'
 )
+
+# Creating cnn
+from tf.keras.models import Sequential
+from tf.keras.layers as layers
+
+cnn = Sequential()
+cnn.add(layers.Conv2D(filters=32, kernel_size=3, input_shape=[64, 64, 3], activation='relu'))
+
+# Pooling
+cnn.add(layers.MaxPooling2D(pool_size=2, strides=2))
+ 
+# Another conv layer
+cnn.add(layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+
+# Flattening layer
+cnn.add(layers.Flatten())
+
+# Full connection layer
+cnn.add(layers.Dense(units=128, activation='relu'))
+
+# Output layer
+cnn.add(layers.Dense(units=1, activation='sigmoid'))
+
