0ef0b877481fa7935237f5ddfd67114dacb978db
omri wallach <omriwallach1992@gmail.com>
2021-01-25 10:48:07 +0100

    print cnn result

--- cnn/main.py
+++ cnn/main.py
@@ -57,4 +57,5 @@ test_image = image.load_img('./dataset/single_prediction/cat_or_dog_1.jpg',targe
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = cnn.predict(test_image)
-print(result)
+print(training_set.class_indices)
+print('dog' if result[0][0] == 1 else 'cat')
